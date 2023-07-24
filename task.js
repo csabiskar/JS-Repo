d61f9bebc1deba34d192e21c2c21c1c703e4a42c
csabiskar <csabiskar@gmail.com>
2023-07-24 16:09:38 +0530

    finding per month days

--- task.js
+++ task.js
@@ -1,19 +1,19 @@
-var num=parseInt(prompt("enter a number"))
-var gardeSystem=()=>{
-    if(num>=90 && num<=100){
-        console.log("A Grade")
+var month=prompt("enter a number")
+var year=prompt("enter a year")
+var year=()=>{
+    if(month>=1 && month<=12){
+        if(month == 2 && year%400 ==0){
+             console.log("The month has 28 days")        
+        }
+        else if(month ==4 || month ==6 || month ==9 || month ==11 ){
+            console.log("The month has 30 days")
+        }
+        else if(month==1 || month ==3 || month ==5 ||month==7||month ==8 || month ==10 || month ==12){
+            console.log("The month has 31 days")
+        }
     }
-    else if(num>=70 && num<=90){
-        console.log(" B Grade")
-    }
-     else if(num>=50 && num<=70){
-        console.log(" C Grade")
-    }
-     else if(num>=35 && num<=50){
-        console.log(" D Grade")
-    }
-    else{
-        console.log("you failed")
-    }
+     else{
+         console.log("invalid month")
+     }
 }
-gardeSystem()
+year()
