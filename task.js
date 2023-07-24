332d68e6bd80d923e48779220059b363cae97ce0
csabiskar <csabiskar@gmail.com>
2023-07-24 15:31:49 +0530

    grade system

--- task.js
+++ task.js
@@ -1,6 +1,19 @@
-let checkLeapYear=()=>{
-    let num=prompt("enter a number")
-    if(num%4 ===0 & num%100 !=0 || a%400 ==0){
-        console.log(num," is leap year")
+var num=parseInt(prompt("enter a number"))
+var gardeSystem=()=>{
+    if(num>=90 && num<=100){
+        console.log("A Grade")
+    }
+    else if(num>=70 && num<=90){
+        console.log(" B Grade")
+    }
+     else if(num>=50 && num<=70){
+        console.log(" C Grade")
+    }
+     else if(num>=35 && num<=50){
+        console.log(" D Grade")
+    }
+    else{
+        console.log("you failed")
     }
 }
+gardeSystem()
