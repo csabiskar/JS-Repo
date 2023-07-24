1a803c6c8c49235def5fb5536447b24996fc6451
csabiskar <csabiskar@gmail.com>
2023-07-24 16:24:18 +0530

    array

--- task1.js
+++ task1.js
@@ -1,13 +1,2 @@
-var a=30
-var b=-30
-var c=60
- if(a>0 && b<0 && c>0 ){
-   console.log("the sign is +")
-}
-else if(a>0 && b<=-30 && c>0)
-{
-    console.log("the sign is -")
-}
-else{
-    console.log("check ur code")
-}
+let array=[25, false, "green", "apple"]
+console.log(array[0])
