cb6c6772c45c1f77c7acb8ce285fc1c7b7f88e9f
csabiskar <csabiskar@gmail.com>
2023-07-24 15:28:28 +0530

    leap year

--- task.js
+++ task.js
@@ -1,24 +1,6 @@
-var num1=parseInt(prompt("enter a number"))
-var num2=parseInt(prompt("enter a number"))
-var answer=prompt("enter a number")
-var num=(num1,num2,answer)=>{
-    if(answer==="add" || answer==="+"){
-        console.log("Result :",num1+num2)
-    }
-    else if(answer==="subtract" ||answer==="-"){
-        coonsole.log("Result : ",num1-num2)
-    }
-    else if(answer==="multiply" ||answer==="*"){
-        console.log("Result :",num1*num2)
-    }
-    else if(answer==="divide" || answer==="/"){
-        console.log("Result :",num1/num2)
-    }
-    else if(answer==="modulos" || answer==="%"){
-        console.log("Result :",num1%num2)
-    }
-    else{
-        console.log("the expression is invalid")
+let checkLeapYear=()=>{
+    let num=prompt("enter a number")
+    if(num%4 ===0 & num%100 !=0 || a%400 ==0){
+        console.log(num," is leap year")
     }
 }
-num(num1,num2,answer)
